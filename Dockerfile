915464f31ad128089915cd31478e5032ed9f62b0
Chanan Zupnick <chazup@gmail.com>
2019-08-14 00:28:56 -0700

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:latest
+
+COPY package.json /app/package.json
+COPY package-lock.json /app/package-lock.json
+
+WORKDIR /app
+
+RUN npm install
+
+# Copy the rest of the code
+COPY . .
+
+ENTRYPOINT ['npm', 'run']
---
+++ index.js
@@ -0,0 +1,43 @@
+import * as monaco from 'monaco-editor';
+
+self.MonacoEnvironment = {
+	getWorkerUrl: function (moduleId, label) {
+		if (label === 'json') {
+			return './json.worker.bundle.js';
+		}
+		if (label === 'css') {
+			return './css.worker.bundle.js';
+		}
+		if (label === 'html') {
+			return './html.worker.bundle.js';
+		}
+		if (label === 'typescript' || label === 'javascript') {
+			return './ts.worker.bundle.js';
+		}
+		return './editor.worker.bundle.js';
+	}
+}
+
+let editor = monaco.editor.create(document.getElementById('container'), {
+	value: 'gYNG2$C227Y\n',
+});
+
+let compile = () => {
+	let content = editor.getModel().getValue();
+	console.log(content);
+	let request = fetch('http://localhost:8001/compile/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+		},
+		redirect:'follow',
+        body: JSON.stringify({ "code": content })
+	})
+	.then((response) => {
+		console.log(response);
+	});
+}
+
+let compileButton = document.getElementById("compile-button");
+compileButton.addEventListener("click", compile);
